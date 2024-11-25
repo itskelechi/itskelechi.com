1884074677330e85e8ac76822f7a61235c22ab59
itskelechi <149823828+itskelechi@users.noreply.github.com>
2024-11-25 00:50:57 -0500

    fixed contact form & image paths

---
+++ contact.js
@@ -0,0 +1,30 @@
+const { response } = require("express");
+
+const form = document.getElementById("my-form");
+const reply = document.getElementById("my-form-status");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const FormData = new FormData(form);
+    const urlEncodedData = new URLSearchParams();
+    FormData.forEach((value, key) => {
+        urlEncodedData.append(key, value);
+    });
+    fetch(form.action, {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: urlEncodedData
+    }).then(response =>{
+        if (response.status === 200){
+            form.reset();
+            reply.style.display = "block";
+        } else{
+            alert("Oops! There was a problem submitting your form. Please try again")
+        }
+    }).catch(err =>{
+        console.err('Error: ', err);
+        alert("Thanks for your message! We'll talk soon. xoxo K")
+    });
+    
+});
+  
--- server.js
+++ server.js
@@ -7,16 +7,29 @@ const PORT = 7693;
 // app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 // app.set('view engine', 'hbs');
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.render('index.html', { title: "We've Been Waiting For You" }));
-app.get('/home', (req, res) => res.render('home.html', { title: 'Heyyy Girlie!!!' }));
-app.get('/intro', (req, res) => res.render('intro.html', { title: 'a lil about me👋🏿' }));
-app.get('/projects', (req, res) => res.render('projects.html', { title: "K's Portfolio ✨" }));
-app.get('/contactus', (req, res) => res.render('contactme.html', { title: "Tell me Soemthing i dont't know" }));
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  });
+  
+//   app.get('/home', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'home.html'));
+//   });
+  
+//   app.get('/intro', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'intro.html'));
+//   });
+  
+//   app.get('/projects', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'projects.html'));
+//   });
+  
+  app.get('/contactus', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contactme.html'));
+  });
 
 
 // Start server
